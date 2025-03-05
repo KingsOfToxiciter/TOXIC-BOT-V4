afe454ac8e20fdb65fdd87717096106a0af6c909
HASAN MAHMUD <146573168+KingsOfToxiciter@users.noreply.github.com>
2025-03-05 20:26:15 +0600

    Update and rename bby to bby.js

--- scripts/cmds/bby.js
+++ scripts/cmds/bby.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const baseApiUrl = async () => {
-  const base = await axios.get('https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json');
+  const base = await axios.get('https://raw.githubusercontent.com/KingsOfToxiciter/YouTube-Download/refs/heads/main/hasan.json');
   return base.data.api;
 };
 
