e2c15930b4c93990583fd6f1cfa012756270d96c
HASAN MAHMUD <146573168+KingsOfToxiciter@users.noreply.github.com>
2025-03-23 17:59:28 +0600

    Update kiss2.js

--- scripts/cmds/kiss2.js
+++ scripts/cmds/kiss2.js
@@ -11,7 +11,7 @@ module.exports = {
         role: 0,
         shortDescription: "KISS",
         longDescription: "",
-        category: "funny",
+        category: "fun",
         guide: "{pn}"
     },
 
