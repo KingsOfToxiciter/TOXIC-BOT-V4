a1e33ea02ddd01776aa8c962d55290922ceb0853
HASAN MAHMUD <146573168+KingsOfToxiciter@users.noreply.github.com>
2025-03-29 01:23:45 +0600

    Update imgur.js

--- scripts/cmds/imgur.js
+++ scripts/cmds/imgur.js
@@ -15,22 +15,22 @@ module.exports = {
     },
 
     onStart: async ({ api, event }) => {
-        let link2;
+        let link;
 
         if (event.type === "message_reply" && event.messageReply.attachments.length > 0) {
-            link2 = event.messageReply.attachments[0].url;
+            link = event.messageReply.attachments[0].url;
         } else if (event.attachments.length > 0) {
-            link2 = event.attachments[0].url;
+            link = event.attachments[0].url;
         } else {
             return api.sendMessage('No attachment detected. Please reply to an image.', event.threadID, event.messageID);
         }
 
         try {
 
-    const toxiciter = global.GoatBot.config.api.api;
-            const res = await axios.get(`${toxiciter}/imgur?url=${encodeURIComponent(link2)}`);
-            const link = res.data.data;
-            return api.sendMessage(`\n\n${link}`, event.threadID, event.messageID);
+    const toxiciter = global.GoatBot.config.api.apis;
+            const response = await axios.get(`${toxiciter}/imgur?url=${encodeURIComponent(link)}`);
+            const link2 = response.data.data;
+            return api.sendMessage(`\n\n${link2}`, event.threadID, event.messageID);
         } catch (error) {
             console.error("Error uploading image to Imgur:", error);
             return api.sendMessage("An error occurred while uploading the image to Imgur.", event.threadID, event.messageID);
