b10e594ae6e72764289794e13ae1977f45cce9f8
HASAN MAHMUD <146573168+KingsOfToxiciter@users.noreply.github.com>
2025-03-30 15:35:12 +0600

    Update hug.js

--- scripts/cmds/hug.js
+++ scripts/cmds/hug.js
@@ -2,76 +2,61 @@ const axios = require("axios");
 const fs = require("fs-extra");
 
 module.exports = {
-		config: {
-				name: "hug",
-				version: "1.0",
-				author: "SiAM",
-				countDown: 5,
-				role: 0,
-				shortDescription: {
-						en: "Send a hug gif to one or two mentioned users.",
-				},
-				longDescription: {
-						en: "This command sends a hug gif to one or two mentioned users.",
-				},
-				category: "fun",
-				guide: {
-						en: "To use this command, type /hug followed by one or two user mentions.",
-				},
-		},
+    config: {
+        name: "hug",
+        version: "1.0",
+        author: "♡︎ 𝐻𝐴𝑆𝐴𝑁 ♡︎",
+        countDown: 5,
+        role: 0,
+        shortDescription: { en: "Send a hug GIF." },
+        longDescription: { en: "Sends a hug GIF to one or two mentioned users." },
+        category: "fun",
+        guide: { en: "/hug @user1 [@user2]" },
+    },
 
-		onStart: async function ({
-				api,
-				args,
-				message,
-				event,
-				threadsData,
-				usersData,
-				dashBoardData,
-				globalData,
-				threadModel,
-				userModel,
-				dashBoardModel,
-				globalModel,
-				role,
-				commandName,
-				getLang,
-		}) {
+    onStart: async function ({ api, args, message, event, usersData }) {
+        let uid1 = null;
+        let uid2 = null;
 
+        if (event.type === "message_reply") {
+            uid1 = event.senderID;
+            uid2 = event.messageReply.senderID;
+        } 
+        else if (event.mentions && Object.keys(event.mentions).length > 0) {
+            const mentionedUsers = Object.keys(event.mentions);
+            uid1 = event.senderID;
+            uid2 = mentionedUsers[0];
 
-			let uid1 = event.senderID;
-			let	uid2 = event.messageReply.senderID;
-				const input = args.join(" ");
-     if (event.type === "message_reply")
-				if (event.mentions && Object.keys(event.mentions).length === 2) {
-						uid1 = Object.keys(event.mentions)[0];
-						uid2 = Object.keys(event.mentions)[1];
-				} else if (event.mentions && Object.keys(event.mentions).length === 1) {
-						uid2 = Object.keys(event.mentions)[0];
-				} else {
-						return message.reply("Please mention one or two users to send a hug gif.");
-				}
+            if (mentionedUsers.length > 1) {
+                uid1 = mentionedUsers[0];
+                uid2 = mentionedUsers[1];
+            }
+        }
 
-				const userName1 = await usersData.getName(uid1);
-				const userName2 = await usersData.getName(uid2);
+        if (!uid2) {
+            return message.reply("Please mention at least one user or reply to a message.");
+        }
 
-				const apiUrl = "https://nekos.best/api/v2/hug?amount=1";
-				axios
-						.get(apiUrl)
-						.then(async (response) => {
-								const gifUrl = response.data.results[0].url;
-								const imageResponse = await axios.get(gifUrl, { responseType: "arraybuffer" });
-								const outputBuffer = Buffer.from(imageResponse.data, "binary");
-								fs.writeFileSync(`${uid1}_${uid2}_hug.gif`, outputBuffer);
+        const userName1 = await usersData.getName(uid1);
+        const userName2 = await usersData.getName(uid2);
 
-								message.reply({
-										body: `${userName1} 🤗 ${userName2}`,
-										attachment: fs.createReadStream(`${uid1}_${uid2}_hug.gif`),
-								}, () => fs.unlinkSync(`${uid1}_${uid2}_hug.gif`));
-						})
-						.catch((error) => {
-								console.log(error);
-								message.reply("There was an error processing the hug gif.");
-						});
-		},
+        const apiUrl = "https://nekos.best/api/v2/hug?amount=1";
+
+        try {
+            const response = await axios.get(apiUrl);
+            const gifUrl = response.data.results[0].url;
+
+            const imageResponse = await axios.get(gifUrl, { responseType: "arraybuffer" });
+            const fileName = `${uid1}_${uid2}_hug.gif`;
+            await fs.promises.writeFile(fileName, imageResponse.data);
+
+            message.reply({
+                body: `${userName1} 🤗 ${userName2}`,
+                attachment: fs.createReadStream(fileName),
+            }, () => fs.unlinkSync(fileName));
+        } catch (error) {
+            console.error(error);
+            message.reply("An error occurred while sending the hug GIF.");
+        }
+    },
 };
